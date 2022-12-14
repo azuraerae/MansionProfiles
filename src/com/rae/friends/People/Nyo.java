efcb57a23d560cf11307464d3125611a401669d0
lenisthemenice <struysx@gmail.com>
2022-12-14 21:03:22 +0100

    added entertainer to nyo

--- src/com/rae/friends/People/Nyo.java
+++ src/com/rae/friends/People/Nyo.java
@@ -4,12 +4,14 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import com.rae.friends.Classes.Person;
+import com.rae.friends.Classes.Role.RoleList;
 import com.rae.friends.Classes.Specie.SpeciesList;
 
 public class Nyo extends Person {
     public Nyo(){
         super(
             "Nyo", 
+            RoleList.entertainer,
             new String[]{
                 "the cat 2.0",
                 "slow",
@@ -19,8 +21,8 @@ public class Nyo extends Person {
             new GregorianCalendar(2001, Calendar.SEPTEMBER, 3).getTime(),
             new String[]{
                 
-            },
-            "entertainer"
+            }
+            
         );
     }
 }
