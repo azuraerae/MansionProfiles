2588ff9b6cf4760d916d3bf4997048e2b92ad252
lenisthemenice <struysx@gmail.com>
2022-12-14 19:49:02 +0100

    I MADE MORE ERRORS

--- src/com/rae/friends/People/Lenny.java
+++ src/com/rae/friends/People/Lenny.java
@@ -4,13 +4,13 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import com.rae.friends.Classes.Person;
-import com.rae.friends.Classes.Spicie.SpiciesList;
+import com.rae.friends.Classes.Species.SpeciesList;
 
 public class Lenny extends Person {
     public Lenny(){
         super(
             "Lenny",
-            SpiciesList.Cat, 
+            SpeciesList.Cat, 
             new String[]{
                 "headless? I get plenty of head",
                 "you better keep your hands off me or you'll catch these hands. MAGIC hands",
